4bd5c7d8c72538dd6ba9b094c8aab3ffccbb5463
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2026-02-03 01:40:42 +0900

    Feat: add lspsaga config

--- lua/plugins/lsp-ui/lspsaga.lua
+++ lua/plugins/lsp-ui/lspsaga.lua
@@ -1,3 +1,37 @@
 return {
-  { "nvimdev/lspsaga.nvim", lazy = false },
+  { 
+    "nvimdev/lspsaga.nvim", 
+    lazy = false,
+    config = function()
+      require("lspsaga").setup({
+        -- UI Config
+        ui = {
+          border = "rounded",
+          code_action = "💡",
+        },
+        -- lightbulb Config
+        lightbulb = {
+          enable = true,
+          sign = true,
+          virtual_text = true,
+        },
+        -- finder Config
+        finder = {
+          keys = {
+            edit = "o",
+            vsplit = "s",
+            split = "i",
+            tabe = "t",
+            quit = "q",
+          },
+        },
+        -- Symbol Outline config
+        outline = {
+          win_width = 40,
+          show_symbol_details = true,
+          auto_preview = true,
+        },
+      })
+    end,
+  },
 }
