06f585cfd4fc7ca42a2e96b5b01dbb6cf0d829ac
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2026-08-06 02:04:27 +0900

    Feat: enable Copilot for Markdown and YAML

--- lua/plugins/copilot/copilot.lua
+++ lua/plugins/copilot/copilot.lua
@@ -2,6 +2,10 @@ return {
   "zbirenbaum/copilot.lua",
   config = function()
     require("copilot").setup({
+      filetypes = {
+        markdown = true,
+        yaml = true,
+      },
       suggestion = {enabled = false},
       panel = {enabled = false},
     })
